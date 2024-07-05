644aba6f0a3af1a0ef8d5dd5a18cd568f9845bdf
Julian Schmid <julian.s.schmid@icloud.com>
2024-07-05 13:53:00 +0200

    Testing Blog

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
+	author: 'JSS', // Site author
+	title: 'JSS Blog', // Site title.
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
+	lang: 'de-DE',
+	ogLocale: 'de_DE',
+	shareMessage: 'Post teilen', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
